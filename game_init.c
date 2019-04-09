4fca20053ec4f38a6ed33f21122f8e930643dec7
f1nbar <43879472+f1nbar@users.noreply.github.com>
2019-04-09 15:16:56 +0100

    Initial commit

---
+++ game_init.c
@@ -0,0 +1,56 @@
+#include "game_init.h"
+#include <stdio.h>
+
+
+/*
+ * This function creates the board for the first time
+ * 
+ * Input: board - a 6x9 array of squares
+ *
+ */
+void initialize_board(square board[NUM_ROWS][NUM_COLUMNS]){
+   
+    for (int i =0; i< NUM_ROWS; i++){
+        for(int j =0; j < NUM_COLUMNS; j++){
+            //creates an obstacle square at positions (0,3), (1,6), (2,4), (3,5), (4,2) and (5,7)
+            if((i == 0 && j==3) || (i == 1 && j == 6) || (i ==2 && j ==4) 
+                    || (i == 3 && j ==5) || (i==4 && j==2) || (i==5 && j==7)){
+                board[i][j].type = OBSTACLE;
+            } else{
+                //creates a normal square otherwise
+                board[i][j].type = NORMAL;
+            }
+            board[i][j].stack = NULL;
+        }
+    }
+}
+    
+    
+ /*
+ * This function creates players for the first time
+ * 
+ * Input: the array of players to be initialized
+ * Output: The number of players of the game
+ */
+int initialize_players(player players[]){
+    int nump;
+    innp:
+    printf("Enter the number of players: \n");
+    scanf("%d",&nump);
+    if(nump < 2 || nump > 6){
+        printf("There must be between 2 and 6 players\n");
+        goto innp;
+    }
+    for(int i=0;i<nump;i++){  
+        int p = 0;
+        printf("Enter Player's name: \n");
+        scanf("%s",players[p]);
+        p++;
+        
+       
+    }
+        return 0;
+}
+    
+   
+     
