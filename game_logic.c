5ba16f9cb7e8049f86c6f87c1bd8c4d7b22f6a04
f1nbar <43879472+f1nbar@users.noreply.github.com>
2019-04-16 21:00:30 +0100

    Place_tokens update
    All squares must now be filled at the start of the game before players can stack their tokens

--- game_logic.c
+++ game_logic.c
@@ -84,6 +84,10 @@ void place_tokens(square board[NUM_ROWS][NUM_COLUMNS], player players[], int num
                 printf("Pick a square between 0 and 5");
                 goto restart;
             }
+            //if statement to make sure all empty squares are filled
+           if(board[selectedSquare][0].stack != 0 && board[0][0].stack == 0 || board[selectedSquare][0].stack != 0 && board[1][0].stack == 0 || board[selectedSquare][0].stack != 0 && board[2][0].stack == 0 || board[selectedSquare][0].stack != 0 && board[3][0].stack == 0 || board[selectedSquare][0].stack != 0 && board[4][0].stack == 0 || board[selectedSquare][0].stack != 0 && board[5][0].stack == 0){
+               printf("You must fil all other squares first\n");
+           }
           
             board[selectedSquare][0].stack=(token *) malloc(sizeof(token));
             board[selectedSquare][0].stack->col = players[j].toke.col;
@@ -103,21 +107,25 @@ void place_tokens(square board[NUM_ROWS][NUM_COLUMNS], player players[], int num
 void play_game(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPlayers){
   printf("****TIME TO PLAY!****\n\n\n");
   
-
+//for loop based on the number of players
   for(int i=0;i<numPlayers;i++){
     int play = i+1;
     int choice;
-    int tokemove,tokedest;
+    int tokemove,tokedest; //assigning variables to move tokens
     printf("Player %d's turn\n",play);
-    printf("Press Y to move token veritcally, N to skip\n");
+    //asks players if they wish to move their token vertically
+    printf("Press Y to move token vertically, N to skip\n");
     scanf(" %c",&choice); 
+    //if statement based on player choice
     if(choice == 'Y'){ 
        printf("Select the square with token of yours to move up or down:\n");
        scanf(" %d",&tokemove);
        printf("Select the destination square with your token:\n");
        scanf(" %d",&tokedest);
+       //moves token on board
        board[tokedest][0].stack = board[tokemove][0].stack;
-       printf("Token Moved!");
+       printf("Token Moved!\n");
+       //prints board
        print_board(board);
     } 
     
